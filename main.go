143e2d62610f32dc9bc219976420acd98058065f
John Machahuay <john.cfmr.2009@gmail.com>
2015-05-08 00:27:05 -0500

    Added version 1

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+	Edad      int
+	Avatar    string
+}
+
+var users map[int]*User
+var i int
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func TodoMainHandler(res http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		firstName := req.FormValue("firstName")
+		lastName := req.FormValue("lastName")
+		edad, _ := strconv.Atoi(req.FormValue("edad"))
+		m := req.MultipartForm
+		files := m.File["avatar"]
+
+		file, err := files[0].Open()
+		defer file.Close()
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		dst, err := os.Create("/Users/johnmachahuay/Documents/programming/go/src/github.com/JohnProg/ex-web1/static/images/" + files[0].Filename)
+		defer dst.Close()
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if _, err := io.Copy(dst, file); err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		user := &User{firstName, lastName, edad, files[0].Filename}
+		users[i] = user
+		i++
+	}
+	err := templates.ExecuteTemplate(res, "base", users)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+func TodoDeleteHandler(res http.ResponseWriter, req *http.Request) {
+	index, _ := strconv.Atoi(req.FormValue("i"))
+	delete(users, index)
+	http.Redirect(res, req, "/", 302)
+}
+func main() {
+	users = make(map[int]*User, 0)
+	i = 1
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", TodoMainHandler)
+	http.HandleFunc("/eliminar", TodoDeleteHandler)
+	http.ListenAndServe(":8000", nil)
+}
